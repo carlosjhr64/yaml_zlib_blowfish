91ec14c09362de6bd38a6d4ca04ffb126d1a757d
carlosjhr64 <carlosjhr64@gmail.com>
2017-07-28 13:29:31 -0700

    must be 16 bytes, truncation.

--- lib/yaml_zlib_blowfish.rb
+++ lib/yaml_zlib_blowfish.rb
@@ -4,9 +4,46 @@ require 'zlib'
 require 'openssl'
 require 'digest/sha2'
 
-# This Gem
-require 'yaml_zlib_blowfish/version.rb'
-require 'yaml_zlib_blowfish/yaml_zlib_blowfish.rb'
+class YamlZlibBlowfish
+  VERSION = '0.1.0'
+
+  # cypher_key = yzb.key
+  attr_accessor :key
+
+  # yzb = YamlZlibBlowfish.new(passphrase)
+  def initialize(passphrase)
+    @key = Digest::SHA256.digest(passphrase)[0..15]
+  end
+
+  # encrypted_string = yzb.cipher(:encrypt, plain_string)
+  # plain_string = yzb.cipher(:decrypt, encrypted_string)
+  def cipher(mode, data)
+    #cipher = OpenSSL::Cipher.new('bf-cbc').send(mode)
+  cipher = OpenSSL::Cipher::BF.new(:CBC).send(mode)
+    cipher.key = @key
+    cipher.update(data) << cipher.final
+  end
+
+  # plain_structure = yzb.decrypt(encrypted_compressed_dump)
+  def decrypt(encrypted)
+    YAML.load(Zlib::Inflate.inflate(cipher(:decrypt, encrypted)))
+  end
+
+  # encrypted_compressed_dump = yzb.encrypt(plain_structure)
+  def encrypt(plain)
+    cipher(:encrypt, Zlib::Deflate.deflate(YAML.dump(plain)))
+  end
+
+  # plain_structure = yzb.load(filename)
+  def load(dumpfile)
+    decrypt(File.read(dumpfile))
+  end
+
+  # yzb.dump(filename, plain_structure)
+  def dump(dumpfile, data)
+    File.write(dumpfile, encrypt(data))
+  end
+end
 
 # Requires:
 #`ruby`
--- lib/yaml_zlib_blowfish/version.rb
+++
@@ -1,3 +0,0 @@
-class YamlZlibBlowfish
-  VERSION = '0.0.1'
-end
